c915138462cfe431825d8fdb52297300c178ca63
phungkhaccuong <cuong.phung@finhay.com.vn>
2020-04-10 15:01:00 +0700

    add info

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+console.log(path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
